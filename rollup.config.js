d9e6d4fc3a88df15f420e1778fc844f40e3b0a7a
Ruslan Kyba <kybargr@gmail.com>
2022-08-24 14:56:02 +0300

    docs: storybook update

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,48 @@
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import webWorkerLoader from 'rollup-plugin-web-worker-loader';
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import webWorkerLoader from "rollup-plugin-web-worker-loader";
 
-import pkg from './package.json';
+import pkg from "./package.json";
 
-const INPUT_FILE_PATH = 'src/index.js';
-const OUTPUT_NAME = 'RectQrScanner';
+const INPUT_FILE_PATH = "src/index.js";
+const OUTPUT_NAME = "RectQrScanner";
 
 const GLOBALS = {
-  react: 'React',
-  'react-dom': 'ReactDOM',
-  'prop-types': 'PropTypes',
+  react: "React",
+  "react-dom": "ReactDOM",
 };
 
 const PLUGINS = [
   babel({
-    babelHelpers: 'runtime',
-    exclude: 'node_modules/**',
+    babelHelpers: "runtime",
+    exclude: "node_modules/**",
   }),
   resolve({
     browser: true,
-    resolveOnly: [
-      /^(?!react$)/,
-      /^(?!react-dom$)/,
-      /^(?!prop-types)/,
-    ],
+    resolveOnly: [/^(?!react$)/, /^(?!react-dom$)/],
   }),
   commonjs(),
 ];
 
-const EXTERNAL = [
-  'react',
-  'react-dom',
-  'prop-types',
-];
+const EXTERNAL = ["react", "react-dom"];
 
 // https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
-const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@baberk\/runtime/, /@zxing\/library/);
+const CJS_AND_ES_EXTERNALS = EXTERNAL.concat(/@babel\/runtime/);
 
 const OUTPUT_DATA = [
   {
     file: pkg.browser,
-    format: 'umd',
+    format: "umd",
   },
   {
     file: pkg.main,
-    format: 'cjs',
-    exports: 'default',
+    format: "cjs",
+    exports: "default",
   },
   {
     file: pkg.module,
-    format: 'es',
+    format: "es",
   },
 ];
 
@@ -64,12 +55,13 @@ const config = OUTPUT_DATA.map(({ file, format, exports }) => ({
     name: OUTPUT_NAME,
     globals: GLOBALS,
   },
-  external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
-  plugins: [webWorkerLoader({
-    targetPlatform: 'browser',
-    external: ['cjs', 'es'].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
-    inline: !['cjs', 'es'].includes(format),
-  })].concat(...PLUGINS),
+  external: ["cjs", "es"].includes(format) ? CJS_AND_ES_EXTERNALS : EXTERNAL,
+  plugins: [
+    webWorkerLoader({
+      targetPlatform: "browser",
+      inline: true,
+    }),
+  ].concat(...PLUGINS),
 }));
 
 export default config;
